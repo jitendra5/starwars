b21f31275c7d450df6993c4798b08a76e881463a
jitendra5 <jitendra.jjr@gmail.com>
2018-05-30 12:53:55 +0530

    images added

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./films.component.css']
 })
 export class FilmsComponent implements OnInit {
-images= [11];
-  constructor() { }
+  images = [];
+  constructor() {}
 
   ngOnInit() {
-    
+    for (var i = 1; i < 11; i++) {
+      this.images.push('Star-Wars-' + i + '.jpeg');
+    }
+    console.log(this.images);
   }
-
 }
