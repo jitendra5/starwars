e8fbd8bcf4ada1a26bab7181e1902a35eb40e619
jitendra5 <jitendra.jjr@gmail.com>
2018-05-30 20:19:42 +0530

    first page complete design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainscreenComponent } from './mainscreen/mainscreen.component';
 import { FilmsComponent } from './films/films.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, MainscreenComponent, FilmsComponent],
+  declarations: [AppComponent, NavbarComponent, MainscreenComponent, FilmsComponent, FooterComponent],
   imports: [BrowserModule,  AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
